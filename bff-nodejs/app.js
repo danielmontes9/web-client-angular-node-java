1ba84e132d6718dfbf897eebd9febba6cc0d9d6f
Daniel Montes <gdanielmontesh9@gmail.com>
2025-07-09 23:27:26 -0600

    feat: add proxy server

--- bff-nodejs/app.js
+++ bff-nodejs/app.js
@@ -1,41 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
+const express = require('express');
+const logger = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+const TARGET = process.env.TARGET_BACKEND;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+if (!TARGET) {
+  console.error("TARGET_BACKEND is not defined in .env");
+  process.exit(1);
+}
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+function proxy(path) {
+  return createProxyMiddleware({
+    target: TARGET,
+    changeOrigin: true,
+    pathRewrite: (pathReq) => pathReq.replace(/^\/(auth|resource)/, '/$1'),
+    onProxyReq: (proxyReq, req) => {
+      if (req.body && Object.keys(req.body).length) {
+        const bodyData = JSON.stringify(req.body);
+        proxyReq.setHeader('Content-Type', 'application/json');
+        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        proxyReq.write(bodyData);
+        proxyReq.end();
+      }
+    },
+    onError(err, req, res) {
+      console.error('Proxy error:', err);
+      if (!res.headersSent) {
+        res.status(500).json({ error: "Proxy error", message: err.message });
+      }
+    },
+  });
+}
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// Routes
+app.post('/auth/login', proxy('/auth/login'));
+app.post('/auth/refresh', proxy('/auth/refresh'));
+app.get('/resource', proxy('/resource'));
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Fallback 404
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not Found' });
 });
 
 module.exports = app;
